cbdf7025f4b14a4f0a1d6553d438cc1119972f2f
Bryce <ImperialEagle1998@gmail.com>
2020-10-31 16:01:54 -0500

    First version of Bank Encapsulation Exercise, has User Input methods utilized.

---
+++ Bank_Account_Software/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_Encapsulation
+{
+    class BankAccount
+    {
+        private double _balance = 0;
+
+
+        public double Balance {
+       
+            get
+                {
+                return _balance;
+            }
+
+            set
+            {
+                _balance = value;
+            }
+
+        }
+
+
+    
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+        public double Deposit(double DepositAmount)
+        {
+
+            Balance =+ DepositAmount;
+            return Balance;
+        }
+
+        public void Withdraw(double WithdrawAmount)
+        {
+            Balance = -WithdrawAmount;
+        }
+
+
+    }
+}
---
+++ Bank_Account_Software/Program.cs
@@ -0,0 +1,93 @@
+﻿using Bank_Encapsulation;
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Bank_Account_Software
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Alabama National Bank! My name is Ann-puter.. get it?... eh, most don't. \n What is Your last name?");
+
+            string lastname = Console.ReadLine();
+            
+
+            Console.WriteLine("And Your gender?");
+            string gender = Console.ReadLine().ToLower();
+
+
+            if (gender == "male")
+            {
+                gender = "Mr.";
+
+            }
+
+            else if (gender == "female ")
+            {
+                gender = "Mrs. ";
+
+            }
+
+
+
+
+
+            Console.WriteLine($"A pleasure to meet Your Aquaintence {gender}{lastname}" );
+
+
+            Console.WriteLine($"What would You like to do today {gender}{lastname} ?You can check Your current balance, deposit or withdraw. Just type in the singular action You would like to conduct.");
+
+            bool conditional = true;
+            string userinput = Console.ReadLine().ToLower();
+
+
+            if (userinput == "balance" )
+            {
+                Console.WriteLine($"Alrighty {gender}{lastname}, Your current balance is... ");
+                double useramount = new BankAccount().GetBalance();
+                Console.WriteLine($"${useramount} dollars.");
+            }
+
+
+            else if (userinput == "deposit")
+            {
+                Console.WriteLine($"Alright {gender}{lastname}, How much would You Like to add to Your Account?");
+
+                string amounttodeposit = Console.ReadLine();
+                double convertedamount = Double.Parse(amounttodeposit);
+                
+                double ndeposit = new BankAccount().Deposit(convertedamount);
+                Console.WriteLine($"Alrighty then, So Your account now has ${ndeposit} dollars inside of it.");
+
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("what?");
+                conditional = false;
+
+            }
+
+
+
+        }
+
+
+       
+           
+
+           
+            
+
+        
+            
+
+        }
+
+
+    }
+
